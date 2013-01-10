0d7a953b5504673af73b3d38d03017fcfa4028f6
Abel Osorio <osorioabel@Abels-MacBook-Pro.local>
2013-01-10 16:50:01 -0430

    rollback

--- application/logs/log-2013-01-10.php
+++ application/logs/log-2013-01-10.php
@@ -3,3 +3,6 @@
 ERROR - 2013-01-10 21:03:54 --> Succesfull Login 
 ERROR - 2013-01-10 21:04:06 --> Severity: Warning  --> Missing argument 3 for Libreta_Model::getlibreta(), called in /Applications/XAMPP/xamppfiles/htdocs/evernoteUcab/application/controllers/libreta.php on line 236 and defined /Applications/XAMPP/xamppfiles/htdocs/evernoteUcab/application/models/libreta_model.php 124
 ERROR - 2013-01-10 21:04:06 --> Severity: Notice  --> Undefined variable: username /Applications/XAMPP/xamppfiles/htdocs/evernoteUcab/application/models/libreta_model.php 128
+ERROR - 2013-01-10 21:07:27 --> Severity: Warning  --> Missing argument 3 for Libreta_Model::getlibreta(), called in /Applications/XAMPP/xamppfiles/htdocs/evernoteUcab/application/controllers/libreta.php on line 236 and defined /Applications/XAMPP/xamppfiles/htdocs/evernoteUcab/application/models/libreta_model.php 124
+ERROR - 2013-01-10 21:07:27 --> Severity: Notice  --> Undefined variable: username /Applications/XAMPP/xamppfiles/htdocs/evernoteUcab/application/models/libreta_model.php 128
+ERROR - 2013-01-10 21:07:32 --> Succesfull Login 
